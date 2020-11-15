6d982b73b402d5e63934308604add2b7c5bdad86
Mindaugas <gelumind@gmail.com>
2020-11-15 13:02:07 +0200

    Initial commit

---
+++ src/test/java/lt/gelumind/personallibrarybackend/PersonallibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.gelumind.personallibrarybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonallibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
